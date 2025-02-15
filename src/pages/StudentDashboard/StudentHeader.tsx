0ae723cd0ab63d0a3ecd8c53cf657f79655e1e2e
KoTerryHtay <luaye1990@gmail.com>
2025-02-15 18:42:57 +0630

    Course DropDown Menu

--- src/pages/StudentDashboard/StudentHeader.tsx
+++ src/pages/StudentDashboard/StudentHeader.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+import CourseDropdownMenu from './course-dropdown-menu';
+
 export default function StudentHeader() {
+  const [courseId, setCourseId] = useState(0);
+  console.log('courseId >>>', courseId);
+
   return (
     <header className="h-20 w-full flex justify-between items-center px-2 ">
       <div className="text-xl font-bold">My Courses</div>
 
       <div className="flex flex-row gap-2 justify-between items-center">
-        <div className="border-[1px] border-black rounded-md p-1 m-2 ">
-          All courses
-        </div>
+        <CourseDropdownMenu onId={setCourseId} />
         <div className="border-[1px] border-black rounded-md p-1 m-2 ">
           Marketing
         </div>
