86ca12b3f047047b370bce56f7123211322f478f
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-02-15 12:39:33 +0630

    :sparkles: feat(cz-commit changes): cz-commit

--- .cz-config.js
+++ .cz-config.js
@@ -1,68 +1,63 @@
-export default {
-  types: [
-    {
-      value: ":sparkles: feat",
-      name: "✨ feat:\tAdding a new feature",
-    },
-    {
-      value: ":bug: fix",
-      name: "🐛 fix:\tFixing a bug",
-    },
-    {
-      value: ":memo: docs",
-      name: "📝 docs:\tAdd or update documentation",
-    },
-    {
-      value: ":lipstick: style",
-      name: "💄 style:\tAdd or update styles, ui or ux",
-    },
-    {
-      value: ":recycle: refactor",
-      name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature",
-    },
-    {
-      value: ":zap: perf",
-      name: "⚡️ perf:\tCode change that improves performance",
-    },
-    {
-      value: ":white_check_mark: test",
-      name: "✅ test:\tAdding tests cases",
-    },
-    {
-      value: ":truck: chore",
-      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
-    },
-    {
-      value: ":rewind: revert",
-      name: "⏪️ revert:\tRevert to a commit",
-    },
-    {
-      value: ":construction: wip",
-      name: "🚧 wip:\tWork in progress",
-    },
-    {
-      value: ":construction_worker: build",
-      name: "👷 build:\tAdd or update regards to build process",
-    },
-    {
-      value: ":green_heart: ci",
-      name: "💚 ci:\tAdd or update regards to build process",
-    },
-  ],
-  messages: {
-    type: "Select the type of change that you're committing:",
-    scope: "\nDenote the SCOPE of this change (optional):",
-    customScope: "Denote the SCOPE of this change:",
-    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: "List any BREAKING CHANGES (optional):\n",
-    footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
-    confirmCommit: "Are you sure you want to proceed with the commit above?",
+export const types = [
+  {
+    value: ":sparkles: feat",
+    name: "✨ feat:\tAdding a new feature",
   },
-
-  allowCustomScopes: true,
-  allowBreakingChanges: ["feat", "fix"],
-  skipQuestions: ["footer", "breaking"],
-  subjectLimit: 100,
+  {
+    value: ":bug: fix",
+    name: "🐛 fix:\tFixing a bug",
+  },
+  {
+    value: ":memo: docs",
+    name: "📝 docs:\tAdd or update documentation",
+  },
+  {
+    value: ":lipstick: style",
+    name: "💄 style:\tAdd or update styles, ui or ux",
+  },
+  {
+    value: ":recycle: refactor",
+    name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature",
+  },
+  {
+    value: ":zap: perf",
+    name: "⚡️ perf:\tCode change that improves performance",
+  },
+  {
+    value: ":white_check_mark: test",
+    name: "✅ test:\tAdding tests cases",
+  },
+  {
+    value: ":truck: chore",
+    name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
+  },
+  {
+    value: ":rewind: revert",
+    name: "⏪️ revert:\tRevert to a commit",
+  },
+  {
+    value: ":construction: wip",
+    name: "🚧 wip:\tWork in progress",
+  },
+  {
+    value: ":construction_worker: build",
+    name: "👷 build:\tAdd or update regards to build process",
+  },
+  {
+    value: ":green_heart: ci",
+    name: "💚 ci:\tAdd or update regards to build process",
+  },
+];
+export const scopes = [];
+export const scopeOverrides = {
+  fix: [
+    { name: "merge" },
+    { name: "style" },
+    { name: "test" },
+    { name: "hotfix" },
+  ],
 };
+export const allowCustomScopes = true;
+export const allowBreakingChanges = ["feat", "fix"];
+export const skipQuestions = ["footer", "breaking"];
+export const subjectLimit = 100;
